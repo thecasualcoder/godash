e9684d449cfbe3417757a2a058c975a8e115af21
Prabhu Jayakumar <j.prabhu91@gmail.com>
2019-10-24 12:48:32 +0530

    Support maps in Map
    Signed-off-by: Prabhu Jayakumar <j.prabhu91@gmail.com>

--- map.go
+++ map.go
@@ -28,9 +28,6 @@ func Map(in, out, mapperFn interface{}) error {
 	}
 
 	mapperFnType := mapper.Type()
-	if mapperFnType.NumIn() != 1 {
-		return fmt.Errorf("mapper function has to take only one argument")
-	}
 
 	if mapperFnType.NumOut() != 1 {
 		return fmt.Errorf("mapper function should return only one return value")
@@ -40,6 +37,11 @@ func Map(in, out, mapperFn interface{}) error {
 		if output.Elem().Kind() != reflect.Slice {
 			return fmt.Errorf("output should be a slice for input of type slice")
 		}
+
+		if mapperFnType.NumIn() != 1 {
+			return fmt.Errorf("mapper function has to take only one argument")
+		}
+
 		if input.Type().Elem() != mapper.Type().In(0) {
 			return fmt.Errorf("mapper function's first argument (%s) has to be (%s)", mapper.Type().In(0), input.Type().Elem())
 		}
@@ -52,6 +54,38 @@ func Map(in, out, mapperFn interface{}) error {
 			arg := input.Index(i)
 
 			returnValues := mapper.Call([]reflect.Value{arg})
+
+			result = reflect.Append(result, returnValues[0])
+		}
+		output.Elem().Set(result)
+
+		return nil
+	}
+
+	if input.Kind() == reflect.Map {
+		if output.Elem().Kind() != reflect.Slice {
+			return fmt.Errorf("output should be a slice for input of type slice")
+		}
+
+		if mapperFnType.NumIn() != 2 {
+			return fmt.Errorf("mapper function has to take exactly two arguments")
+		}
+
+		if mapper.Type().In(0) != input.Type().Key() {
+			return fmt.Errorf("mapper function's first argument (%s) has to be (%s)", mapper.Type().In(0), input.Type().Key())
+		}
+		if mapper.Type().In(1) != input.Type().Elem() {
+			return fmt.Errorf("mapper function's second argument (%s) has to be (%s)", mapper.Type().In(1), input.Type().Elem())
+		}
+		if mapper.Type().Out(0) != output.Elem().Type().Elem() {
+			return fmt.Errorf("mapper function's return type has to be (%s) but is (%s)", mapper.Type().Out(0), output.Elem().Type().Elem())
+		}
+
+		result := reflect.MakeSlice(output.Elem().Type(), 0, input.Len())
+		for _, key :=  range input.MapKeys() {
+			value := input.MapIndex(key)
+
+			returnValues := mapper.Call([]reflect.Value{key, value})
 
 			result = reflect.Append(result, returnValues[0])
 		}
